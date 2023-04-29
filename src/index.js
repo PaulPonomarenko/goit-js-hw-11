cb1a632f0b5a014870548a08b9b9e1ef669195e1
PaulPonomarenko <pashaponomarenkoa1999@gmail.com>
2023-04-29 07:50:10 +0300

    добавляет оформление

--- src/index.js
+++ src/index.js
@@ -44,16 +44,16 @@ function addMarkup(hits) {
   <a class = "gallery__link" href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
   <div class="info">
     <p class="info-item">
-      <b>Likes:${likes}</b>
+      <b>Likes: ${likes}</b>
     </p>
     <p class="info-item">
-      <b>Views:${views}</b>
+      <b>Views: ${views}</b>
     </p>
     <p class="info-item">
-      <b>Comments:${comments}</b>
+      <b>Comments: ${comments}</b>
     </p>
     <p class="info-item">
-      <b>Downloads:${downloads}</b>
+      <b>Downloads: ${downloads}</b>
     </p>
   </div>
 </div>`;
